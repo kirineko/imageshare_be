99b168114229e2ccdd4bfbcd92d5421fbeb094e1
kirineko <kirineko@qq.com>
2023-10-14 13:54:08 +0800

    add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
+FROM maven as builder
+COPY . /workspace
+WORKDIR /workspace
+RUN mvn package
+
+
 FROM openjdk:17
 WORKDIR /demo
-COPY target/demo-0.0.1-SNAPSHOT.jar demo.jar
+COPY --from=builder /workspace/target/demo-0.0.1-SNAPSHOT.jar demo.jar
 #设置镜像的时区,避免出现8小时的误差
 ENV TZ=Asia/Shanghai
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
---
+++ build.sh
@@ -0,0 +1,14 @@
+set -ex
+
+if [ ! -n "$1" ]; then
+    VER=`git tag | grep -v "v" | tail -n 1 |  python3 -c "import sys; vers = sys.stdin.readline().strip().split('.'); print('.'.join([*vers[:2], str(int(vers[2])+1)]))"`
+else
+    VER="$1"
+fi
+git tag $VER
+git push -u origin ${VER}
+
+#docker buildx build --platform linux/amd64 -t registry.cn-guangzhou.aliyuncs.com/seig/imagesite:$VER .
+docker build -t registry.cn-guangzhou.aliyuncs.com/seig/imagesite:$VER .
+docker push registry.cn-guangzhou.aliyuncs.com/seig/imagesite:$VER
+echo registry.cn-guangzhou.aliyuncs.com/seig/imagesite:$VER
